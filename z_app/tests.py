9b533ab1311d734847d336dffe2c1a24de975253
Your Name <niran360@gmail.com>
2021-04-27 21:13:23 +0100

    crud

--- z_app/tests.py
+++ z_app/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from .models import Post
 
 # Create your tests here.
+class BlogTests(TestCase):
+    def setup(self):
+        self.user = get_user_model().objects.create_user(
+            username = 'testuser',
+            email = 'niran360@gmail.com',
+            password = 'password'
+        )
+
+        self.post = Post.objects.create(
+            title = 'A good day to code',
+            body = 'Everyday is a good day to code if you want',
+            author = self.user
+        )
+
+def test_string_representation(self):
+    post = Post(title = 'A great day')
+    self.assertEqual(str(post), post.title)
+
+def test_post_content(self):
+    self.assertEqual(f'{self.post.title}', 'A great day indeed')
+    self.assertEqual(f'{self.post.author}', 'testuser')
+    self.assertEqual(f'{self.post.body}', 'Nice body')
+
+def test_post_list_view(self):
+    response = self.client.get(reverse('home'))
+    self.assertEqual(response.status_code, 200)
+    self.assertContains(response, 'Nice body')
+    self.assertTemplateUsed(response, 'home.html')
+
+def test_post_detail_view(self):
+    response = self.client.get('/post/1/')
+    no_response = self.client.get('/post/100000/')
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(no_response.status_code, 404)
+    self.assertContains(response, 'good title')
+    self.assertTemplateUsed(response, 'poast_detail.html')
--- z_app/urls.py
+++ z_app/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 urlpatterns = [
-    path('index/', views.index, name ="index")
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name= 'post_delete'),
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name= 'post_edit'),
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('', BlogListView.as_view(), name='home'),
+
 ]
